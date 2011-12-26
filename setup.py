7fe53514748b9bfbf4f110b5c614791858698163
Yuri Prezument <y.prezument@gmail.com>
2011-12-26 18:08:10 +0200

    setup.py infos

--- setup.py
+++ setup.py
@@ -2,8 +2,15 @@ from distutils.core import setup
 
 setup(
     name='django-logentry-admin',
+    author='Yuri Prezument',
+    author_email='y@yprez.com',
     version='0.0.1dev',
-    packages=['django-logentry-admin', ],
-    license='GPL',
+    packages=['django-logentry-admin'],
+    license='GPL3',
+    #url='http://pypi.python.org/pypi/django-logentry-admin/',
+    description='Add Django LogEntries the the Django admin site',
     long_description=open('README.txt').read(),
+    install_requires=[
+        'Django >= 1.1.1',
+    ],
 )
