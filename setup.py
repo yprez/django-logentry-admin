c539da4ecb6d01ce78623bfa674e6e47ca3fa596
Yuri Prezument <y.prezument@gmail.com>
2011-12-26 18:14:14 +0200

    Added GPL3 license

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author_email='y@yprez.com',
     version='0.0.1dev',
     packages=['django-logentry-admin'],
-    license='GPL3',
+    license='LICENSE.txt',
     #url='http://pypi.python.org/pypi/django-logentry-admin/',
     description='Add Django LogEntries the the Django admin site',
     long_description=open('README.rst').read(),
