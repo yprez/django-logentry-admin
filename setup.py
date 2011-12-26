580c98c9a37f80381fc6c44443f48659cdd039fc
Yuri Prezument <y.prezument@gmail.com>
2011-12-26 15:02:22 +0200

    Added setup.py (basic)

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='django-logentry-admin',
+    version='0.0.1dev',
+    packages=['django-logentry-admin', ],
+    license='GPL',
+    long_description=open('README.txt').read(),
+)
