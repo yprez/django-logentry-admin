7fc5d3c1dd54f0f738b1b8d42f22492c3cc7034c
Yuri Prezument <y.prezument@gmail.com>
2011-12-26 21:23:26 +0200

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='django-logentry-admin',
     author='Yuri Prezument',
     author_email='y@yprez.com',
-    version='0.1.0dev',
+    version='0.1.0',
     packages=['logentry_admin'],
     license='LICENSE.txt',
     url='http://github.com/yprez/django-logentry-admin',
