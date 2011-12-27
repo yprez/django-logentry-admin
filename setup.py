d3773f61ebf86a0b271c991aa5476177d9735082
Yuri Prezument <y.prezument@gmail.com>
2011-12-27 13:39:28 +0200

    Added classifiers

--- setup.py
+++ setup.py
@@ -13,4 +13,15 @@ setup(
     install_requires=[
         'Django >= 1.3',
     ],
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
 )
