8fb6bf5b3a2676b63b456cf1385ebf00592a8d05
Yuri Prezument <y@yprez.com>
2014-09-28 00:25:06 +0300

    Add coverage

--- runtests.sh
+++ runtests.sh
@@ -2,7 +2,9 @@
 
 export PYTHONPATH=".:$PYTHONPATH"
 export DJANGO_SETTINGS_MODULE="test_settings"
+export DJANGO_ADMIN_CMD=`which django-admin.py`
 
-# django-admin.py syncdb --no-input
+django-admin.py syncdb --no-input
 django-admin.py migrate
-django-admin.py test logentry_admin
+coverage run --source=logentry_admin $DJANGO_ADMIN_CMD test logentry_admin
+coverage report --show-missing
