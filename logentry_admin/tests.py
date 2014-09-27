34665f43efd63ba51c9fda114a79b1641aa2fd30
Yuri Prezument <y@yprez.com>
2014-09-28 01:57:19 +0300

    Test object links

--- logentry_admin/tests.py
+++ logentry_admin/tests.py
@@ -1,8 +1,11 @@
+from django.contrib.admin.sites import AdminSite
 from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase, Client
 
-from django.contrib.admin.models import LogEntry
-from django.contrib.contenttypes.models import ContentType
+from django.contrib.admin.models import LogEntry, ADDITION, CHANGE, DELETION
+from logentry_admin.admin import LogEntryAdmin
+
 
 User = get_user_model()
 
@@ -31,8 +34,30 @@ class LogentryAdminTests(TestCase):
         ContentType.objects.get_for_model(User)
         LogEntry.objects.log_action(
             user_id=self.user.id, content_type_id=ContentType.objects.get_for_model(User).id,
-            object_id=self.user.id, object_repr=repr(self.user), action_flag=2)
+            object_id=self.user.id, object_repr=repr(self.user), action_flag=CHANGE)
 
         res = client.get('/admin/admin/logentry/')
         self.assertEquals(res.status_code, 200)
         self.assertIn('Log entries', res.content)
+
+    def test_object_link(self):
+        admin = LogEntryAdmin(LogEntry, AdminSite())
+        deleted = LogEntry(object_repr='OBJ_REPR', action_flag=DELETION)
+        self.assertEquals(admin.object_link(deleted), 'OBJ_REPR')
+
+        created = LogEntry(
+            content_type_id=ContentType.objects.get_for_model(User).id,
+            action_flag=ADDITION,
+            object_id=self.user.id,
+            object_repr='OBJ_REPR'
+        )
+        self.assertIn('OBJ_REPR', admin.object_link(created))
+        self.assertIn('<a href="', admin.object_link(created))
+
+        no_reverse = LogEntry(
+            content_type_id=ContentType.objects.get(name='session').id,
+            action_flag=CHANGE,
+            object_id=5,
+            object_repr='OBJ_REPR'
+        )
+        self.assertEquals(admin.object_link(no_reverse), 'OBJ_REPR')
