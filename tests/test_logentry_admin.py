ca83f1b24680cd7014cc19b8c0c8691b0d3104da
Yuri Prezument <y@yprez.com>
2016-01-24 11:52:36 +0200

    Test admin filters

--- tests/test_logentry_admin.py
+++ tests/test_logentry_admin.py
@@ -26,6 +26,23 @@ def test_admin_logentry_list_view(admin_client, admin_user):
     assert 'Log entries' in res.content.decode()
 
 
+def test_admin_logentry_list_view_filters(admin_client, admin_user):
+    LogEntry.objects.log_action(
+        user_id=admin_user.id,
+        content_type_id=ContentType.objects.get_for_model(User).id,
+        object_id=admin_user.id,
+        object_repr=repr(admin_user),
+        action_flag=CHANGE
+    )
+
+    res = admin_client.get('/admin/admin/logentry/', {
+        'action_flag': CHANGE,
+        'user': admin_user.id,
+    })
+    assert res.status_code == 200
+    assert 'Log entries' in res.content.decode()
+
+
 def test_object_link(admin_user):
     admin = LogEntryAdmin(LogEntry, AdminSite())
     deleted = LogEntry(object_repr='OBJ_REPR', action_flag=DELETION)
