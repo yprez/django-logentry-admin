d9164704bf8237d1722572d94a5ba2e8fe04b0be
Yuri Prezument <y@yprez.com>
2017-12-14 14:52:16 +0200

    Update setup.py classifiers

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Natural Language :: English',
@@ -43,5 +44,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
