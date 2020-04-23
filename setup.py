cf42122aa4e82c22bb26140921bb7468df159636
Yuri Prezument <y@yprez.com>
2020-04-23 14:07:12 +0300

    Update classifiers

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Natural Language :: English',
@@ -50,5 +51,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
