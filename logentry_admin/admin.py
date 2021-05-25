62711d46ab8ffcf07b0def481dfaf82b389e7dba
Yuri Prezument <y@yprez.com>
2021-05-25 17:23:16 +0300

    And more cleanup

--- logentry_admin/admin.py
+++ logentry_admin/admin.py
@@ -1,6 +1,3 @@
-from __future__ import unicode_literals
-
-import django
 from django.contrib import admin
 from django.contrib.admin.models import LogEntry, ADDITION, CHANGE, DELETION
 from django.contrib.auth import get_user_model
